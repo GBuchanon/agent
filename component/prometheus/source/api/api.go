54bfc92d8a29fc136cceb8592b9ea724240dc06b
Piotr Gwizdala <17101802+thampiotr@users.noreply.github.com>
2023-05-02 10:58:28 +0100

    fixing the metrics duplicate registration
    go fix
    
    WIP
    
    wip tests
    
    More tests

--- component/prometheus/source/api/api.go
+++ component/prometheus/source/api/api.go
@@ -3,17 +3,19 @@ package api
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"reflect"
+	"sync"
+
 	"github.com/go-kit/log/level"
 	"github.com/grafana/agent/component"
-	"github.com/grafana/agent/component/prometheus"
+	agentprom "github.com/grafana/agent/component/prometheus"
 	"github.com/grafana/agent/pkg/util"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/storage/remote"
 	"github.com/weaveworks/common/logging"
 	"github.com/weaveworks/common/server"
-	"net/http"
-	"reflect"
-	"sync"
 )
 
 func init() {
@@ -26,15 +28,36 @@ func init() {
 	})
 }
 
+type Arguments struct {
+	HTTPAddress string               `river:"http_address,attr"`
+	HTTPPort    int                  `river:"http_port,attr"`
+	ForwardTo   []storage.Appendable `river:"forward_to,attr"`
+}
+
+type Component struct {
+	opts               component.Options
+	handler            http.Handler
+	serverErr          chan error
+	fanout             *agentprom.Fanout
+	uncheckedCollector *util.UncheckedCollector
+
+	updateMut sync.RWMutex
+	args      Arguments
+	server    *server.Server
+}
+
 func New(opts component.Options, args Arguments) (component.Component, error) {
-	fanout := prometheus.NewFanout(args.ForwardTo, opts.ID, opts.Registerer)
+	fanout := agentprom.NewFanout(args.ForwardTo, opts.ID, opts.Registerer)
+	uncheckedCollector := util.NewUncheckedCollector(nil)
 	c := &Component{
-		opts:      opts,
-		handler:   remote.NewWriteHandler(opts.Logger, fanout),
-		serverErr: make(chan error),
-		fanout:    fanout,
-		args:      args,
+		opts:               opts,
+		handler:            remote.NewWriteHandler(opts.Logger, fanout),
+		serverErr:          make(chan error),
+		fanout:             fanout,
+		args:               args,
+		uncheckedCollector: uncheckedCollector,
 	}
+	opts.Registerer.MustRegister(uncheckedCollector)
 
 	// we do not need to hold the lock here, since `c` is not yet exposed
 	err := c.createNewServer(args)
@@ -43,23 +66,6 @@ func New(opts component.Options, args Arguments) (component.Component, error) {
 	}
 
 	return c, nil
-}
-
-type Arguments struct {
-	HTTPAddress string               `river:"http_address,attr"`
-	HTTPPort    int                  `river:"http_port,attr"`
-	ForwardTo   []storage.Appendable `river:"forward_to,attr"`
-}
-
-type Component struct {
-	opts      component.Options
-	handler   http.Handler
-	serverErr chan error
-	fanout    *prometheus.Fanout
-
-	updateMut sync.RWMutex
-	args      Arguments
-	server    *server.Server
 }
 
 func (c *Component) Run(ctx context.Context) error {
@@ -109,8 +115,12 @@ func (c *Component) runServer() {
 	s := c.server
 	c.updateMut.RUnlock()
 
+	if s == nil { // already shut down
+		return
+	}
+
 	err := s.Run()
-	level.Warn(c.opts.Logger).Log("msg", "server Run ended", "error", err)
+	level.Warn(c.opts.Logger).Log("msg", "server Run exited", "error", err)
 	if err != nil {
 		select {
 		case c.serverErr <- err:
@@ -122,14 +132,23 @@ func (c *Component) runServer() {
 // createNewServer will create a new server.Server and assign it to the server field.
 // It is not goroutine-safe and a updateMut write lock should be held when it's called.
 func (c *Component) createNewServer(args Arguments) error {
-	s, err := server.New(c.serverConfigForArgs(args))
+	config := c.serverConfigForArgs(args)
+
+	// [server.Server] registers new metrics every time it is created. To
+	// avoid issues with re-registering metrics with the same name, we create a
+	// new registry for the server every time we create one, and pass it to an
+	// unchecked collector to bypass uniqueness checking.
+	serverRegistry := prometheus.NewRegistry()
+	c.uncheckedCollector.SetCollector(serverRegistry)
+	config.Registerer = serverRegistry
+
+	s, err := server.New(config)
 	if err != nil {
 		return fmt.Errorf("failed to create server: %v", err)
 	}
 
 	s.HTTP.Path("/api/v1/metrics/write").Methods("POST").HandlerFunc(c.handler.ServeHTTP)
 	c.server = s
-
 	return nil
 }
 
@@ -145,12 +164,10 @@ func (c *Component) shutdownServer() {
 
 func (c *Component) serverConfigForArgs(args Arguments) server.Config {
 	return server.Config{
-		MetricsNamespace:  "prometheus_source_api",
-		HTTPListenAddress: args.HTTPAddress,
-		HTTPListenPort:    args.HTTPPort,
-		Log:               logging.GoKit(c.opts.Logger),
-		// TODO: We're unable to use weaveworks server's metrics - see https://github.com/grafana/agent/issues/3646
-		Registerer:              util.NewNoopRegisterer(),
+		MetricsNamespace:        "prometheus_source_api",
+		HTTPListenAddress:       args.HTTPAddress,
+		HTTPListenPort:          args.HTTPPort,
+		Log:                     logging.GoKit(c.opts.Logger),
 		RegisterInstrumentation: false,
 	}
 }
